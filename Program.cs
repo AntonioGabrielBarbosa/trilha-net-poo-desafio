7fae580f85ddd612c323aa1d2d7b296106c13a8d
AntonioGabrielBarbosa <antonio.gabrielbarbosa2003@hotmail.com>
2025-12-17 21:45:15 -0300

    Resolução do Desafio da Trilha .NET com POO.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+// IMPLEMENTADO !
+Iphone iphone = new Iphone("123456789", "iPhone 13", "IMEI123456", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
+Nokia nokia = new Nokia("987654321", "Nokia 3310", "IMEI987654", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
